6b3fa4fbb512ad3f24651a9ae70b876b04e21511
youerning <673125641@qq.com>
2016-06-08 17:03:03 +0800

    beta 0.0.1 complete

--- handler.py
+++ handler.py
@@ -6,6 +6,23 @@ import sys
 import string
 import requests
 import re
+from random import randint
+from functools import wraps
+import ConfigParser
+
+
+
+
+
+###config file read###
+Conf= ConfigParser.ConfigParser()
+Conf.readfp(open("config.ini"))
+YoudaoKey = Conf.get("datasource","YoudaoKey")
+YoudaoKeyFrom = Conf.get("datasource","YoudaoKeyFrom")
+BaiduAPI = Conf.get("datasource","BaiduAPI")
+
+print BaiduAPI
+print type(YoudaoKeyFrom)
 
 reload(sys) 
 sys.setdefaultencoding('utf8')
@@ -18,14 +35,12 @@ ___metatype__ = type
 
 Menu = """
 help(帮助菜单):help，？，帮助
-fy(翻译):fy 内容,如:fy 你好
-tq(天气):tq 地名，如:tq 上海
-xh(笑话):xh
-gq(歌曲): gq 歌曲名
-rb(每周日报):日报  获取每天日报
-kd(快递):kd 快递公司 内容，如:kd 韵达 快递单号
-bk(百度百科):bk 内容，如:bk 中国
-bd(百度):bk 内容，如:bd 中国
+fy(翻译):fy 内容,如:翻译 你好
+tq(天气):tq 地名，如:天气 上海
+xh(笑话):笑话
+kd(快递):快递 快递公司 快递单号，如:kd 韵达 快递单号
+bk(百度百科):百科 内容，如:百科 中国
+bd(百度):百度 内容，如:百度 中国
 """
 
 Menu2 = """
@@ -46,10 +61,15 @@ bd(百度):bk 内容，如:bd 中国
 ----现在暂时只支持fy,tq,kd,bk----
 """
 
+
+
 KDCode = {"申通":"shentong","EMS":"ems","顺丰":"shunfeng",
 	"圆通":"yuantong","中通":"zhongtong","韵达":"yunda",
 	"天天":"tiantian","汇通":"huitongkuaidi","全峰":"quanfengkuaidi",
 	"德邦":"debangwuliu","宅急送":"zhaijisong"}
+
+MenuCode = {"翻译":"fy","天气":"tq","笑话":"xh",
+            "快递":"kd","百科":"bk","百度":"bd"}
 
 def RequestTextGet(PostXml):
 
@@ -67,9 +87,15 @@ def RequestTextGet(PostXml):
 
     return ToUser,FromUser,UserContent
 
-def ConfigGet(name):
-    pass
-
+def ArgsConfig(Args=1): 
+    def deco(func):
+        @wraps(func)
+        def wrap(self,*args,**kwargs):
+            args = args[:Args]
+            ret = func(self,*args,**kwargs)
+            return ret
+        return wrap
+    return deco
 
 class Handler():
     """
@@ -83,16 +109,25 @@ class Handler():
     #    if callable(func):return Func(*args)
     def __init__(self,Ret=None):
         self.Ret = Ret
+    
+
 
     def Get(self,Action,*args):
+        if Action in MenuCode: Action = MenuCode[Action]
         Func = getattr(self,Action,None)
         if callable(Func):self.Ret = Func(*args)
+
     def help(slef):
         return Menu
+    
+    @ArgsConfig(1)
+    def test(self,*args,**kwargs):
+        print args,kwargs
 
 
 class TextHandler(Handler):
-    def fy(self,Word,KeyFrom="youerning",Key="16283712"):
+    @ArgsConfig(1)
+    def fy(self,Word,KeyFrom=YoudaoKeyFrom,Key=YoudaoKey):
         Payload = {"key":Key,"keyfrom":KeyFrom,"q":Word}
         ReqURL = u'http://fanyi.youdao.com/openapi.do?type=data&doctype=json&version=1.1'
         YoudaoRet = requests.get(ReqURL,params=Payload)
@@ -117,14 +152,16 @@ class TextHandler(Handler):
         else:
             return "查询内容有问题"
 
+    @ArgsConfig(1)
     def bd(self,Word):
         Word = urllib2.quote(Word.encode("utf8"))
         ReqURL = "http://www.baidu.com/s?wd=" + Word
 
-        return ReqURL
+        return "点击下面链接\n" + ReqURL
 
 
-    def tq(self,City,ApiKey="47885eaa7687f444901013c25f4b7745"):
+    @ArgsConfig(1)
+    def tq(self,City,ApiKey=BaiduAPI):
         ReqURL = 'http://apis.baidu.com/heweather/weather/free'
         Payload = {"city":City}
 
@@ -133,10 +170,12 @@ class TextHandler(Handler):
         TQRet = requests.post(ReqURL,data=Payload,headers=header)
         RetJSON = TQRet.json()
 
-        if RetJSON["HeWeather data service 3.0"][0]["status"] == "ok":
-            RetCond =  RetJSON["HeWeather data service 3.0"][0]["now"]["cond"]["txt"]
-            RetBrf = RetJSON["HeWeather data service 3.0"][0]["suggestion"]["comf"]["brf"] 
-            RetBrfTXT = RetJSON["HeWeather data service 3.0"][0]["suggestion"]["comf"]["txt"]
+        k = "HeWeather data service 3.0"
+        
+        if  k in RetJSON.keys() and RetJSON[k][0]["status"] == "ok":
+            RetCond =  RetJSON[k][0]["now"]["cond"]["txt"]
+            RetBrf = RetJSON[k][0]["suggestion"]["comf"]["brf"] 
+            RetBrfTXT = RetJSON[k][0]["suggestion"]["comf"]["txt"]
 
             Ret = "\n".join([RetCond,RetBrf,RetBrfTXT])
                 
@@ -146,6 +185,7 @@ class TextHandler(Handler):
         return Ret
 
 
+    @ArgsConfig(2)
     def kd(self,CpyName,PostID):
         CpyName = KDCode[CpyName]
         print CpyName,PostID
@@ -168,8 +208,9 @@ class TextHandler(Handler):
         return Ret
 
 
+    @ArgsConfig(1)
     def bk(self,Word):
-        Payload = {"bk_key":Word}
+        Payload = {"bk_key":Word.encode("utf8")}
         ReqURL= "http://baike.baidu.com/api/openapi/BaikeLemmaCardApi?format=json&appid=379020"
         BKRet = requests.get(ReqURL,params=Payload)
         print BKRet.url
@@ -185,7 +226,13 @@ class TextHandler(Handler):
             Ret = "该词条暂未收录"
             
         return Ret
-   def xh(self):
-       XHResp = requests.get("http://www.qiushibaike.com/text/")
-       XHRet = re.findall(r"""<div class="content">(\s*.*\s*)</div>""",XHResp)
 
+    @ArgsConfig(0)
+    def xh(self):
+        XHResp = requests.get("http://www.qiushibaike.com/text/")
+        XHRet = re.findall(r"""<div class="content">(\s*.*\s*)</div>""",XHResp.content)
+        num = randint(0,len(XHRet))
+
+        Ret = XHRet[num]
+
+        return Ret
